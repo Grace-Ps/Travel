2d1c22b13335fa4ebe190a18ccfafa3f7aa78a67
Grace-Ps <106224296+Grace-Ps@users.noreply.github.com>
2023-08-16 17:11:59 +0530

    lmlm

--- Travelproject/Travelproject/urls.py
+++ Travelproject/Travelproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for Travelproject project.
+URL configuration for travelproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from travelproject import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path('',include('Travelapp.urls')),
+    path('admin/', admin.site.urls),
+    path('',include('travelapp.urls')),
+    path('credentials/',include('credentials.urls'))
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
